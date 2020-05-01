7aaf60bb9fb0d2e424c27210312c8dcfb8e7fceb
Flavius Brandeu <flavius.brandeu@gmail.com>
2020-05-01 18:23:44 +0300

    Created the graphic interface (a form) for creating a new tournament

---
+++ TrackerUI/CreateTournamentForm.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TrackerUI
+{
+    public partial class CreateTournamentForm : Form
+    {
+        public CreateTournamentForm()
+        {
+            InitializeComponent();
+        }
+
+        private void CreateTournamentForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void teamOneScoreLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void teamOneScoreValue_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void entryFeeValue_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void scoreButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void selectTeamLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void createNewTeamLink_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+
+        }
+
+        private void selectTeamDropDown_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void createTournamentButton_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
