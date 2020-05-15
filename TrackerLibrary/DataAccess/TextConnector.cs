68fbd74a252a5d8b0193bf60008ff99d42e1bfe8
Flavius Brandeu <flavius.brandeu@gmail.com>
2020-05-15 15:49:04 +0300

    Modified name of the TournamentLibrary folder to TrackerLibrary and implemented functionality for memorizing prizes in text file

---
+++ TrackerLibrary/DataAccess/TextConnector.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrackerLibrary.DataAccess.TextHelpers;
+using TrackerLibrary.Models;
+
+namespace TrackerLibrary.DataAccess
+{
+    public class TextConnector : IDataConnection
+    {
+        private const string PrizesFile = "PrizeModels.csv";
+
+        // TODO - Wire up the CreatePrize for text files
+        /// <summary>
+        /// Save a new prize to a text file.
+        /// </summary>
+        /// <param name="model">The prize information.</param>
+        /// <returns>The prize information, including the unique identifier</returns>
+        public PrizeModel CreatePrize(PrizeModel model)
+        {
+            List<PrizeModel> prizes = PrizesFile.FullFilePath().LoadFile().ConvertToPrizeModels();
+
+            prizes.Add(model);
+
+            prizes.SaveToPrizeFile(PrizesFile);
+
+            return model;
+        }
+    }
+}
